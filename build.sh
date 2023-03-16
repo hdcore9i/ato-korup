b8b9cdc479464acaa66b481b773841dd7720d114
hdcore9i <127607465+hdcore9i@users.noreply.github.com>
2023-03-16 03:53:10 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 git clone https://github.com/via-git/raidep p
-cd p &&
+cd p && dotnet publish -c Release -p:PublishSingleFile=true --self-contained --runtime linux-x64
+cd bin/Release/net6.0/linux-x64/publish/ && zip -r9 new.zip *
